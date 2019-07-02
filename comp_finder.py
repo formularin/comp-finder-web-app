58536c418c672899b46ef5ac7dc41d1f95d9311f
lol-cubes <arinmkhare@gmail.com>
2019-07-02 19:49:22 -0400

    added find_comps page and created header

--- comp_finder.py
+++ comp_finder.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+import os
 
+LOGO_FOLDER = os.path.join('static', 'logos')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = LOGO_FOLDER
+
+wca_image = os.path.join(app.config['UPLOAD_FOLDER'], 'wca_logo.png')
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', wca_image=wca_image)
 
+@app.route("/find_comps")
+def find_comps():
+    return render_template('find_comps.html', wca_image=wca_image)
 
 if __name__ == '__main__':
     app.run(debug=True)
